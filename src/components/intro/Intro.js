805e20f32457d22f574a0b6224173fb3cfe659ae
felipezintzun <felipezintzun@gmail.com>
2022-04-07 12:39:57 -0500

    added icons that hyperlink to github/linkedin

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./intro.css";
 import Me from "../../img/felipe-zintzun-2.jpg";
+import {FaGithubSquare, FaLinkedin} from 'react-icons/fa';
 
 function Intro () {
     return (
@@ -21,10 +22,18 @@ function Intro () {
                     <div className="i-description">
                     Felipe is a full stack developer with experience in graphics and coding. He recently received a certificate from the UT Austin Coding Bootcamp for completing the course. Felipe gradutated from Texas State University with a Bachelor's of Science, majoring in Advertising and minoring in Art & Design. He finds joy in all things but not limited to cutting-edge design, cooking, technology, street tacos, craft cocktails, his maltipoo Lilo, and all things futuristic.
                 </div>
+                <div className="icon-wrapper">
+                <a className='i-icon' href="https://www.linkedin.com/in/felipe-mendoza-zintzun-40b28a232/" target="_blank"><FaLinkedin size='50'/></a>
+                <a className='i-icon' href="https://github.com/felipezintzun" target="_blank"> <FaGithubSquare size='50'/></a>
                 </div>
+
+            </div>
             </div>
             <div className="i-right">
-                <img src={Me} alt="" className="i-image" />
+                <div className="i-card bg"></div>
+                    <div className="i-card">
+                        <img src={Me} alt="" className="i-image" />
+                    </div>
             </div>
 
         </div>
---
+++ src/components/portfolio/Portfolio.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./portfolio.css";
+
+
+
+function Portfolio () {
+    return (
+        <div>
+            card
+        </div>
+    )
+}
+
+export default Portfolio
