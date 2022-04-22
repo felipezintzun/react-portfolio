1c5ffde8f0c73f34e3617e74366ae8d9a760035c
felipezintzun <felipezintzun@gmail.com>
2022-04-21 20:07:52 -0500

    added final project to portfolio

--- src/data.js
+++ src/data.js
@@ -2,35 +2,40 @@ export const projects = [
 {
     id: 1,
     img: "https://github.com/felipezintzun/react-portfolio/blob/main/src/img/teksess.png?raw=true",
-    link: "https://teksess.herokuapp.com/",
+    link: "https://github.com/felipezintzun/flower-power",
 },
 {
     id: 2,
+    img: "https://github.com/felipezintzun/react-portfolio/blob/main/src/img/teksess.png?raw=true",
+    link: "https://teksess.herokuapp.com/",
+},
+{
+    id: 3,
     img: "https://github.com/felipezintzun/react-portfolio/blob/main/src/img/coding-quiz.png?raw=true",
     link: "https://felipezintzun.github.io/code-quiz/",
 },
 {
-    id: 3,
+    id: 4,
     img: "https://github.com/felipezintzun/react-portfolio/blob/main/src/img/watchopedia.png?raw=true",
     link: "https://felipezintzun.github.io/watchopedia/",
 },
 {
-    id: 4,
+    id: 5,
     img: "https://github.com/felipezintzun/react-portfolio/blob/main/src/img/work-day-scheduler.png?raw=true",
     link: "https://felipezintzun.github.io/work-day-scheduler/",
 },
 {
-    id: 5,
+    id: 6,
     img: "https://github.com/felipezintzun/react-portfolio/blob/main/src/img/run-buddy.png?raw=true",
     link: "https://felipezintzun.github.io/run-buddy/",
 },
 {
-    id: 6,
+    id: 7,
     img: "https://github.com/felipezintzun/react-portfolio/blob/main/src/img/note-taker.png?raw=true",
     link: "https://guarded-plains-54212.herokuapp.com/notes",
 },
 {
-    id: 7,
+    id: 8,
     img: "https://github.com/felipezintzun/react-portfolio/blob/main/src/img/taskmaster-pro.png?raw=true",
     link: "https://felipezintzun.github.io/taskmaster-pro/",
 }
